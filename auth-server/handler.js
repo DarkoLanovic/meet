1fc8d5fb4646ca7531c8a179f43b0630f5241943
Darko Lanovic <lanovic.darko@gmail.com>
2021-12-14 18:40:04 -0500

    initial commit

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -102,7 +102,7 @@ module.exports.getCalendarEvents = async (event) => {
       statusCode:200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true'
+        
       },
       body: JSON.stringify({ events: results.data.items }),
     };
