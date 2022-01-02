24c0cf250342583790d0c180cefd9d01493403ff
Darko Lanovic <lanovic.darko@gmail.com>
2022-01-02 08:58:41 -0500

    initial commit

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,23 +4,15 @@ import React, { Component } from "react";
 class NumberOfEvents extends Component {
   state = {
     query: 32,
-    maxNumber: '',
   }
 
   updateNumberOfEvents = (event) => {
     const value = event.target.value
     this.props.udateEvent(null, value);
-    if(value < 1 || value > 32) {
-      this.setState({
-        query: value,
-        maxNumber: "Please enter number between 1 - 32",
-      })
-    } else {
     return this.setState({
       query: value,
-      maxNumber: '',
     })
-    }
+    
   }
   
   render() {
