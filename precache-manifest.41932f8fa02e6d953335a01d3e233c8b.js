cb29be11b25cacd452c8f8f9b4c2060f108e52c6
Darko Lanovic <lanovic.darko@gmail.com>
2022-01-05 12:52:46 -0500

    Updates

---
+++ precache-manifest.41932f8fa02e6d953335a01d3e233c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd1e1e53e5287c0b8991",
+    "url": "/meet/static/css/main.8bfc5947.chunk.css"
+  },
+  {
+    "revision": "dd1e1e53e5287c0b8991",
+    "url": "/meet/static/js/main.dd1e1e53.chunk.js"
+  },
+  {
+    "revision": "b087d70075fbc48ee96a",
+    "url": "/meet/static/js/1.b087d700.chunk.js"
+  },
+  {
+    "revision": "a61751184a2b285ea8e1",
+    "url": "/meet/static/js/2.a6175118.chunk.js"
+  },
+  {
+    "revision": "d50d296ba1b5712e91af",
+    "url": "/meet/static/js/runtime~main.d50d296b.js"
+  },
+  {
+    "revision": "81fda99eaee8f5be4e60f1a25ee475fa",
+    "url": "/meet/index.html"
+  }
+];
