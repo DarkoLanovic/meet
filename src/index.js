c84f0f0c3006e5a21e52bca8c40c93043770abda
Darko Lanovic <lanovic.darko@gmail.com>
2022-01-19 11:43:03 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,7 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('eac6a4ac51574708bdad035561baeaa5').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
