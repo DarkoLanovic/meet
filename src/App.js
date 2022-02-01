57813ff1ab008b0b10b302112c4c501f97786506
Darko Lanovic <lanovic.darko@gmail.com>
2022-02-01 11:45:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import WelcomeScreen from './WelcomeScreen';
+import { get } from 'lodash';
 
 
 
@@ -16,44 +18,43 @@ class App extends Component {
     numberOfEvents: 32,
     currentLocation: 'all',
     infoText: '',
+    // This state "showWelcomeScreen" will be used as a ﬂag to determine when to render the welcome screen as follows: 
+    //  true will mean “show the welcome screen,” 
+    //  false will mean “hide it to show the other components,” 
+    //  undeﬁned will be used to render an empty div until the state gets either true or false
+    showWelcomeScreen: undefined,
   }
 
   // to load events when the app loads - to make the API call and save inital data to state (?)
-  // componentDidMount() {
-  //   const { numberOfEvents } = this.state;
-  //   this.mounted = true;
-  //   getEvents().then((events) => {
-  //     if (this.mounted) {
-  //       this.setState({ 
-  //         events: events.slice(0, numberOfEvents), 
-  //         locations: extractLocations(events) 
-  //       });
-  //     }
-  //   });
-  // }
-
   componentDidMount() {
     const { numberOfEvents } = this.state;
     this.mounted = true;
-    getEvents().then((events) => {
-      if(!navigator.onLine) {
-        if (this.mounted) {
-          this.setState({ 
-            infoText: '⚠️ Warning: No internet connection. Events may not be up to date', 
-            events: events.slice(0, numberOfEvents), 
-            locations: extractLocations(events) 
-          });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        this.setState({ events, locations: extractLocations(events) });
+        if(!navigator.onLine) {
+          if (this.mounted) {
+            this.setState({ 
+              infoText: '⚠️ Warning: No internet connection. Events may not be up to date', 
+              events: events.slice(0, numberOfEvents), 
+              locations: extractLocations(events) 
+            });
+          }
+        } else {
+          if (this.mounted) {
+            this.setState({ 
+              events: events.slice(0, numberOfEvents), 
+              locations: extractLocations(events) 
+            });
+          }
         }
-      } else {
-        if (this.mounted) {
-          this.setState({ 
-            events: events.slice(0, numberOfEvents), 
-            locations: extractLocations(events) 
-          });
-        }
-      }
-
-    });
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -82,7 +83,13 @@ class App extends Component {
   }
 
 
+  // -------- RENDER --------
   render() {
+    
+    // Adding a conditional that renders an empty "div" element
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />
+
     return (
       // This displays all the components within the App component
       <div className="App">
@@ -102,6 +109,10 @@ class App extends Component {
         
         {/* Pass the state to EventList as a prop of events */}
         <EventList events={this.state.events} /> 
+
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} 
+        />
        
       </div>
     );
--- src/api.js
+++ src/api.js
@@ -73,7 +73,7 @@ const removeQuery = () => {
 
 
 // This function takes the "accessToken" you found and checks whether it’s a valid token or not
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
