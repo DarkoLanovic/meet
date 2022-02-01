bf788c4e0b27a02d098034583f82b040c15549c6
Darko Lanovic <lanovic.darko@gmail.com>
2022-02-01 11:53:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     const { numberOfEvents } = this.state;
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const isTokenValid = (checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
