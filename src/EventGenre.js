25394aa07e3406c621566d1eb8e7a9a47fbd3f41
Darko Lanovic <lanovic.darko@gmail.com>
2022-02-05 16:37:28 -0500

    initial commit

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,7 +3,7 @@ import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
-  const colors = ['#E0AC00', '#7C9BB6', '#7BB79D', '#4281A4', '#DE5456'];
+  const colors = ['#F5B800', '#7C9BB6', '#7BB79D', '#4281A4', '#DE5456'];
   useEffect(() => { setData(() => getData());}, [events]);
 
   const getData = () => {
